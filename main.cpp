d16e5694eb214fd825bad310828a90004bd4dbc1
Sheep Sun <sunxiaoyang2003@gmail.com>
2022-10-06 19:49:52 +0800

    Add initial PoC

--- main.cpp
+++ main.cpp
@@ -10,6 +10,37 @@ int main(){
     SprdMmioDJtagInterface jtag;
 
     jtag.InitializeChain();
-//    cout << devmem_readl(0x20900280) << endl;
+
+    jtag.ResetToIdle();
+//    cout << "IDCODE of device 0: " << jtag.GetIDCode(0) << endl;
+    uint8_t wdata[4] = {0};
+	uint8_t rdata[4] = {0};
+    uint8_t wdatadr[4] = {0};
+	uint8_t rdatadr[4] = {0};
+
+    wdata[3] = 0x72; // Core version
+    jtag.EnterShiftIR();
+    jtag.ShiftData(true, wdata, rdata, 32);
+    jtag.LeaveExit1IR();
+//    printf("Data of IR : %x  \n", *(uint32_t*)(rdata));
+
+    jtag.EnterShiftDR();
+    jtag.ShiftData(true, wdatadr, rdatadr, 32);
+	jtag.LeaveExit1DR();
+
+    printf("Core version : %x\n", *(uint32_t*)(rdatadr));
+
+
+    wdata[3] = 0x34; // PC value (RO)
+    jtag.EnterShiftIR();
+    jtag.ShiftData(true, wdata, rdata, 32);
+    jtag.LeaveExit1IR();
+
+    jtag.EnterShiftDR();
+    jtag.ShiftData(true, wdatadr, rdatadr, 32);
+	jtag.LeaveExit1DR();
+
+    printf("Current PC value : %x\n", *(uint32_t*)(rdatadr));
+
     return 0;
 }
